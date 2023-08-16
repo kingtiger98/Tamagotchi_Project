0588052b364fca246774a018c80a5cfb14554099
Jaeha Hwang <wogk216@naver.com>
2023-08-16 21:13:34 +0900

    이름 변경 적용되게 생명주기 활용해 개선

--- TamagotchiProject/TamagotchiProject/SettingViewController.swift
+++ TamagotchiProject/TamagotchiProject/SettingViewController.swift
@@ -25,6 +25,8 @@ class SettingViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        settingTableView.reloadData()
+        
     }
 
 }
